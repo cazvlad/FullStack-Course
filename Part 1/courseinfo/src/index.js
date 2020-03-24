45fdbf80f0c354d3e77bb2cbbe28105dd6bc0c8e
Vlad Cazacu <vcazacu@u.rochester.edu>
2020-03-24 00:28:58 -0400

    1.4
    Up until 1.4

--- Part 1/courseinfo/src/index.js
+++ Part 1/courseinfo/src/index.js
@@ -1,60 +1,74 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const coursename = 'Half Stack application development'
-const part1 = 'Fundamentals of React'
-const part2 = 'Using props to pass data'
-const part3 = 'State of a component'
-const exercises1 = 10
-const exercises2 = 7
-const exercises3 = 14
-
-const Header = (course) => {
+const Header = (props) => {
+  console.log(props)
   return (
     <div>
       <h1>
-        {course.name}
+        {props.course}
       </h1>
     </div>
   )
 }
 
-const Part = (prop) => {
+const Part = (props) => {
+  console.log(props)
   return (
     <div>
       <p>
-      {prop.part} {prop.exercises}
+      {props.name} {props.exercises}
       </p>
     </div>
   )
 }
 
-const Content = () => {
+const Content = (props) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1}/>
-      <Part part={part2} exercises={exercises2}/>
-      <Part part={part3} exercises={exercises3}/>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </div>
   )
 }
 
-const Total = () => {
+const Total = (props) => {
   return (
     <div>
       <p>
-        Number of exercises {exercises1 + exercises2 + exercises3}
+        Number of exercises {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises}
       </p>
     </div>
   )
 }
 
 const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  parts.forEach(value => {
+    console.log(value)
+  })
+
   return (
     <div>
-      <Header name={coursename}/>
-      <Content />
-      <Total />
+      <Header course={course}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
