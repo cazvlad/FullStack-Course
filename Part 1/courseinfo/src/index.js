5f20bb10cdbeaecd90c6b0ce0f58a68615652dfb
Vlad Cazacu <vcazacu@u.rochester.edu>
2020-03-28 16:56:40 -0400

    exercise 1.5
    updated as to 1.5

--- Part 1/courseinfo/src/index.js
+++ Part 1/courseinfo/src/index.js
@@ -24,6 +24,7 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return (
     <div>
       <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
@@ -44,31 +45,33 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
-  parts.forEach(value => {
+  course.parts.forEach(value => {
     console.log(value)
   })
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
